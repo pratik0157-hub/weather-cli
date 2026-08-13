888f8d03d437e4e74320cdf0fb9469d734c5947b
pratik0157-hub <pratik0157@gmail.com>
2026-08-13 15:31:51 +0530

    Make city argument required and update tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,11 +4,13 @@ from cli import parse_arguments
 import pytest
 
 def test_city_is_required():
+    sys.argv = ["main.py"]
+
     with pytest.raises(SystemExit):
-        parse_arguments([])
+        parse_arguments()
 
 def test_parse_arguments():
-    sys.argv = ["main.py", "Pune"]
+    sys.argv = ["main.py","--city", "Pune"]
 
     city = parse_arguments()
 
